647a5b7a58985d131539549dc06d9c00410e7c3c
TaegyuHan <gksxorb147@gmail.com>
2022-12-01 10:16:40 +0900

    2022-12-01

--- chapter13/2.연구소/main.py
+++ chapter13/2.연구소/main.py
@@ -1,8 +1,20 @@
 """
+    인체에 치명적인 바이러스를 연구하던 연구소에서 바이러스가 유출되었다.
+    다행히 바이러스는 아직 퍼지지 않았고, 바이러스의 확산을 막기 위해서
+    연구소에 벽을 세우려고 한다.
 
+    연구소는 크기가 N×M인 직사각형으로 나타낼 수 있으며,
+    직사각형은 1×1 크기의 정사각형으로 나누어져 있다.
+    연구소는 빈 칸, 벽으로 이루어져 있으며, 벽은 칸 하나를 가득 차지한다.
+
+    일부 칸은 바이러스가 존재하며, 이 바이러스는 상하좌우로
+    인접한 빈 칸으로 모두 퍼져나갈 수 있다.
+     새로 세울 수 있는 벽의 개수는 3개이며, 꼭 3개를 세워야 한다.
 """
 from sys import stdin as input
-import heapq
+from copy import deepcopy
+from itertools import combinations
+from collections import deque
 
 MOVE = [
     (-1, 0),
@@ -11,16 +23,52 @@ MOVE = [
     (0, -1),
 ]
 
+WALL = 1
+EMPTY = 0
+VIRUS = 2
+
+
 class P:
 
     def __init__(self, file_name):
         """ 데이터 받기 """
         with open(file_name, encoding="utf-8") as input:
             self._N, self._M = map(int, input.readline().split())
-            self._board = [list(map(int, input.readline().split())) for _ in range(self._N)]
+
+            self._emptys = []
+            self._board = []
+            self._virus = []
+            for row in range(self._N):
+                row_data = list(map(int, input.readline().split()))
+                self._board.append(row_data)
+                for col in range(len(row_data)):
+                    if row_data[col] == EMPTY:
+                        self._emptys.append((row, col))
+                    if row_data[col] == VIRUS:
+                        self._virus.append((row, col))
+
+    def _show_board(self, board):
+        for row in board:
+            print(*row, sep="\t")
+
+    def _spread_virus(self, select_wall):
+        """ 바이러스 퍼지기 """
+        visited = [[0] * self._M for _ in range(self._N)]
+        board = deepcopy(self._board)
+
+        for row, col in select_wall:
+            board[row][col] = WALL
+
+        q = deque(self._virus)
+        while q:
+            crow, ccol = q.popleft()
+            if visited[crow][ccol]: continue
 
     def _logic(self):
         """ 풀이 """
+        for select_wall in combinations(self._emptys, 3):
+            self._spread_virus(select_wall)
+            break
 
     def answer(self, file_name: str = "1.txt") -> None:
         print(self._logic())
@@ -28,6 +76,7 @@ class P:
     def answer_test(self, file_name: str = "1.txt") -> None:
         """ 테스트 정답 출력 """
         return self._logic()
+
 
 if __name__ == '__main__':
     p = P(file_name="./data/input/1.txt")
